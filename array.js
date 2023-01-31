111ae73ba4a2e34cdb98ab0f2687d13a9134c907
farhanthegreat <farhantuber@gmail.com>
2023-01-31 18:50:11 +0600

    array basic added

---
+++ array.js
@@ -0,0 +1,6 @@
+var friendsAge = [11, 21, 45, 17, 14] ;
+var picnicFee = [5000, 2000, 1000];
+var nayikas = ['bubli', 'mahi', 'porimoni', 'shabana'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(friendsAge.length);
