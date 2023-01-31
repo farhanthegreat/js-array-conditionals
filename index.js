68213556d83c7bd609e7b1fb7df663ae8b4f1436
farhanthegreat <farhantuber@gmail.com>
2023-01-31 19:33:02 +0600

    push pop added

---
+++ index.js
@@ -0,0 +1,17 @@
+var numbers = [10, 30, 40, 5, 7, 56];
+
+//get elements value by index
+
+var element = numbers[1]; 
+
+// console.log(element);
+
+//set elements value
+numbers[1]=77;
+numbers[3]=44;
+// console.log(numbers);
+
+//find index of an element
+
+var positionIndex = numbers.indexOf(77);
+console.log(positionIndex);
---
+++ push-pop.js
@@ -0,0 +1,22 @@
+var numbers = [10, 40, 50, 44, 30, 60];
+// numbers.pop();
+console.log(numbers);
+var baad = numbers.pop();
+console.log(baad);
+console.log(numbers);
+numbers.unshift(33);
+console.log(numbers);
+numbers.shift();
+console.log(numbers);
+// numbers.push(99);
+// console.log(numbers);
+
+//use push to add as the last elements in to an array
+var friends = ['balam', 'nahid', 'nasim'];
+friends.push('rafi');
+friends.push('point');
+// console.log(friends);
+
+//use pop to get rid of the last element
+
+
