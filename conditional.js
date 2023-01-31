158a023657f9536d4c99c9f3e05fbfe113179d71
farhanthegreat <farhantuber@gmail.com>
2023-01-31 23:33:17 +0600

    if else added

---
+++ conditional.js
@@ -0,0 +1,35 @@
+var iphonePrice = 70000;
+var myBudget = 90000;
+//if iphone price is less than my budget I will buy the iphone
+/**
+ * if (condition)
+ * {
+ * will execute id the condition is true.
+ * }
+*/
+if(iphonePrice<myBudget)
+{
+    // console.log('iphone kinbo');
+}
+
+var murgi = 500;
+var myMoney = 50;
+
+// if(murgi <= myMoney)
+// {
+//     console.log('Murgir ran khamu and haddi chibamu');
+// }
+// if (murgi>myMoney)
+// {
+//     console.log('Ami alu vorta and daal khabo.');
+// }
+
+if(murgi < myMoney)
+{
+    console.log('I will eat chicken.');
+
+}
+else 
+{
+    console.log('I will eat alu vorta');
+}
