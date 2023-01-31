a59b8cac9ea0e6f9442eead7641dd578eba2cfb4
farhanthegreat <farhantuber@gmail.com>
2023-01-31 19:47:49 +0600

    comparison added

---
+++ comparison.js
@@ -0,0 +1,23 @@
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+// console.log(4 != 6);
+// console.log(5 <= 6);
+// console.log(5 >= 6);
+// console.log(3 >= 1);
+// console.log(3 >= 3);
+
+var num1 = 55;
+var num2 = 44;
+// console.log(num1>num2);
+// console.log(num1>num2);
+// console.log(num1>num2);
+
+var money1 = 20;
+var money2 = 30;
+
+var result1 = 4;
+var result2 = 3;
+
+console.log(money1 > money2 || result1 > result2);
