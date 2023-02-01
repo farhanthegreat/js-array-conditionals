e58210c09a5c7d186817f87866ade59b5d2f7931
farhanthegreat <farhantuber@gmail.com>
2023-02-01 15:04:04 +0600

    conditionals added

---
+++ multiple-condition.js
@@ -0,0 +1,19 @@
+var isGraduated = true;
+var salary = 50000;
+var cars = 1;
+// if (isGraduated === true && salary >= 50000 && cars >=1)
+// {
+//     console.log('I will get married.');
+// }
+// else {
+//     console.log('biya ache ki?');
+// }
+
+if(isGraduated === true || salary > 50000) {
+    console.log('prem kori cholo.');
+}
+else {
+    console.log('prem ki asibe?');
+}
+
+if((isGraduated === true && salary > 50000) || cars >= 1 )
